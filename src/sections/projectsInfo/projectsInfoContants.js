23e20e6620180fbf0d2a2110e46e77e420b177b8
jhonnyfc <jhonnyfc88@gmail.com>
2025-05-03 19:22:09 +0200

    cheap: irish fuel

--- src/sections/projectsInfo/projectsInfoContants.js
+++ src/sections/projectsInfo/projectsInfoContants.js
@@ -1,5 +1,11 @@
 export const projectsInfoList = [
   {
+    projectTitle: "Cheap Irish Fuel",
+    projectLink: "https://cheapirishfuel.info/",
+    urlBigImage: "imgs/cheapirishfuel.png",
+    urlPortraitImage: "imgs/cheapirishfuel.png",
+  },
+  {
     projectTitle: "Senior Frontend Developer",
     projectLink: "https://www.linkedin.com/in/jhonnyfc",
     description: `Sopra Steria <br> Working on frontend developing Progressive Web Applications (PWAs),
